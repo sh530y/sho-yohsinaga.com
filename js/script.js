4711189f7a2d5fef93c8833f340b495ac71f4d42
Sho Yoshinaga <sh530y@gmail.com>
2024-02-24 19:19:51 +0900

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,88 @@
+//
 
+// ナビボタン
+document.querySelector(".nav-btn").addEventListener("click", function () {
+  this.classList.toggle("nav-open");
+  document
+    .querySelectorAll(".header, .nav--global, .nav__inner--global")
+    .forEach(function (element) {
+      element.classList.toggle("nav-open");
+    });
+  setTimeout(function () {
+    document.querySelectorAll(".nav--global, .nav__inner--global").forEach(function (element) {
+      element.classList.toggle("transition");
+    });
+  }, 200);
+});
+
+// リンククリックでナビ閉じる
+document.querySelectorAll(".nav--global a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    document
+      .querySelectorAll(".nav-btn, .header, .nav--global, .nav__inner--global")
+      .forEach(function (element) {
+        element.classList.remove("nav-open");
+      });
+  });
+});
+
+// 画面リサイズ時にクラス除去
+window.addEventListener("resize", function () {
+  let windowWidth = window.innerWidth;
+  if (windowWidth > 960) {
+    document
+      .querySelectorAll(".nav-btn, .header, .nav--global, .nav__inner--global")
+      .forEach(function (element) {
+        element.classList.remove("nav-open", "transition");
+      });
+  }
+});
+
+// ヘッダーカラーチェンジ
+window.addEventListener("scroll", function () {
+  const sliderHeight = document.querySelector(".mv").offsetHeight;
+  if (sliderHeight - 60 < window.pageYOffset) {
+    document.querySelectorAll(".header, .nav-btn__line").forEach(function (element) {
+      element.classList.add("bg-wh");
+    });
+  } else {
+    document.querySelectorAll(".header, .nav-btn__line").forEach(function (element) {
+      element.classList.remove("bg-wh");
+    });
+  }
+});
+
+// ローダーアニメーション
+var bar = new ProgressBar.Circle(loader__circle, {
+  strokeWidth: 1.5,
+  easing: "easeInOut",
+  duration: 1400,
+  color: "#333435",
+  trailWidth: 0,
+  svgStyle: null,
+});
+bar.animate(1.0, function () {
+  $("#loader, #loader__circle").delay(200).fadeOut(800);
+});
+
+// Rellaxオプション
+var rellax = new Rellax(".rellax", {
+  speed: -9.5,
+  center: true,
+  round: true,
+  vertical: true,
+});
+
+// 慣性スクロール
+
+const lenis = new Lenis({
+  lerp: 0.2, // Linear interpolation (lerp) intensity (between 0 and 1)
+  duration: 1, // The duration of scroll animation (in seconds). Useless if lerp defined
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
